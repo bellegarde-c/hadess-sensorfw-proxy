f9e0cf62ec45f9e48baa355007595a483197f407
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2025-02-10 21:29:03 +0100

    m

--- sensorfw-core/sensorfw_common.cpp
+++ sensorfw-core/sensorfw_common.cpp
@@ -100,7 +100,7 @@ const char* repowerd::Sensorfw::plugin_path() const
 bool repowerd::Sensorfw::load_plugin()
 {
     int constexpr timeout_default = 100;
-    auto const error;
+    GError *error;
     auto const result =  g_dbus_connection_call_sync(
             dbus_connection,
             dbus_sensorfw_name,
